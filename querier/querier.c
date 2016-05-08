5ebbe83ee19631700fd146fd35b431abf25a970d
Kyle Dotterrer <kyle.dotterrer.18@dartmouth.edu>
2016-05-08 18:47:18 -0400

    Initial test suggest Querier fully functional

--- querier/querier.c
+++ querier/querier.c
@@ -6,9 +6,6 @@
  *
  =========================================================================== */
 
-/* To use */
-/************* *******************/
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -23,15 +20,36 @@
 /****************** CONSTANTS *******************/
 char *MALLOC_ERROR = "Error: memory allocation error";
 
+/************** TYPE DEFINITIONS ***************/
+typedef struct countersPair {
+	counters_t *intersection;
+	counters_t *ctrs2;
+} countersPair_t;
+
+typedef struct result {
+	int docID;
+	int score;
+} result_t;
+
 /************* Function Prototypes *************/
+static void printFunc(void *arg, int key, int count);
+static counters_t *performQuery(char **tokens, hashtable_t *index);
+static counters_t *intersectCounters(counters_t *ctrs1, counters_t *ctrs2);
+static void intersectHelper(void *arg, int key, int count);
+static void unionCounters(counters_t *ctrs1, counters_t *ctrs2);
+static void unionHelper(void *arg, int key, int count);
+static result_t **sortResults(counters_t *results, int *numResults);
+static void countCounters(void *arg, int key, int count);
+static void insertCounter(void *arg, int key, int count);
+int resultCompare(const void *a, const void *b);
 static char **tokenize(char *query);
 static void freeTokensArray(char **tokens);
 static bool validateQuery(char **tokens);
 static int parseArguments(const int argc, char *argv[]);
 static bool isCrawlerDirectory(char *dir);
 static int lines_in_file(FILE *fp);
-int numDigits(int number);
-int arrayLength(char **array);
+static int numDigits(int number);
+static int arrayLength(char **array);
 static void hashDeleteFunc(void *data);
 
 
@@ -61,24 +79,186 @@ static void hashDeleteFunc(void *data);
 
  	while ( (query = readline(stdin)) != NULL) {
 
+ 		printf("query: %s\n", query);
+
  		char **tokens = tokenize(query);
- 	
- 		if (!tokens)
- 			exit(11);
+ 		if (!tokens) {
+ 			free(query);
+ 			continue;
+ 		}
 
  		if (!validateQuery(tokens)) {
  			freeTokensArray(tokens);
+ 			free(query);
  			continue;
  		}
- 
+
+ 		counters_t *pageResults = performQuery(tokens, index);
+ 		
+ 		counters_iterate(pageResults, printFunc, stdout);
+ 		int numResults = 0;
+ 		result_t **sortedResults = sortResults(pageResults, &numResults);
+
+ 		for (int i = numResults-1; i >= 0; i--){
+ 			printf("%d\n", sortedResults[i]->docID);
+ 		}
+
+
  		freeTokensArray(tokens);
+ 		free(pageResults);
+ 		free(query);
  	}
 
+ 	hashtable_delete(index);
 
  	exit(0);
  }
 
-char **tokenize(char *query) 
+ static void printFunc(void *arg, int key, int count)
+ {
+ 	FILE *fp = arg;
+ 	fprintf(fp, "docID: %d count: %d\n", key, count);
+ }
+
+
+static counters_t *performQuery(char **tokens, hashtable_t *index) 
+{
+	int arrLen = arrayLength(tokens);
+	int i = 0;
+
+	counters_t *finalResults = counters_new();
+
+	while (i < arrLen) {
+
+		counters_t *andSequence = hashtable_find(index, tokens[i]);
+
+		char *word;
+
+		while ( (i < arrLen) && strcmp( ( word = tokens[i] ), "or") != 0 ) {
+			if (strcmp(word, "and") == 0){
+				i++;
+			}
+			else {
+				// find docs with that word, intersect with total andSequence counters
+				counters_t *currWordResults = hashtable_find(index, word);
+				andSequence = intersectCounters(andSequence, currWordResults);
+				i++;
+			}
+		}
+		unionCounters(finalResults, andSequence);
+		free(andSequence);
+		i++;
+	}
+
+	return finalResults;
+}
+
+/*
+ * intersectCounters: intersect ctrs2 with ctrs1, manipulating
+ * ctrs1 in place
+ */
+static counters_t *intersectCounters(counters_t *ctrs1, counters_t *ctrs2)
+{
+	counters_t *intersection = counters_new();
+
+	countersPair_t *pair = count_malloc_assert(sizeof(struct countersPair), MALLOC_ERROR);
+	pair->intersection = intersection;
+	pair->ctrs2 = ctrs2;
+
+
+	counters_iterate(ctrs1, intersectHelper, pair);
+
+	free(pair);
+
+	return intersection;
+}
+
+/*
+ * intersectHelper:
+ */
+static void intersectHelper(void *arg, int key, int count) 
+{
+	countersPair_t *pair  = arg;
+
+	int count2 = counters_get(pair->ctrs2, key);
+
+	if ( count2 != 0) {
+		int minCount = count < count2 ? count : count2;
+		counters_set(pair->intersection, key, minCount);
+	}
+}
+
+/*
+ * unionCounters:
+ */
+static void unionCounters(counters_t *ctrs1, counters_t *ctrs2)
+{
+	counters_iterate(ctrs2, unionHelper, ctrs1);
+}
+
+/*
+ * unionHelper:
+ */
+static void unionHelper(void *arg, int key, int count)
+{
+	counters_t *ctrs1 = arg;
+
+	int sum = count + counters_get(ctrs1, key);
+
+	counters_set(ctrs1, key, sum);
+}
+
+static result_t **sortResults(counters_t *results, int *numResults) 
+{
+
+	counters_iterate(results, countCounters, numResults);
+
+	result_t **sorted = count_calloc(*numResults, sizeof(result_t*));
+
+	counters_iterate(results, insertCounter, sorted);
+
+	qsort(sorted, *numResults, sizeof(result_t*), resultCompare);
+
+	return sorted;
+}
+
+static void countCounters(void *arg, int key, int count)
+{
+	int *countersCount = ((int *)arg);
+
+	(*countersCount)++;
+}
+
+static void insertCounter(void *arg, int key, int count)
+{
+	static int index = 0;
+
+	result_t **results = arg;
+	if (results == NULL || results[0] == NULL){
+		index = 0;
+	}
+	result_t *newResult = count_malloc_assert(sizeof(struct result), MALLOC_ERROR);
+
+	newResult->docID = key;
+	newResult->score = count;
+
+	results[index] = newResult;
+
+	index++;
+}
+
+int resultCompare(const void *a, const void *b)
+{
+	const result_t *res1 = *( (result_t **) a );
+	const result_t *res2 = *( (result_t **) b );
+
+	return (res1->score - res2->score);
+}
+
+/*
+ *
+ */
+static char **tokenize(char *query) 
 {
  	char *word;
  	int wordCount = 0;
@@ -97,7 +277,9 @@ char **tokenize(char *query)
  			
  			if (isalpha(word[i]) == 0) {
  				fprintf(stderr, "Error: bad char '%c' in query\n", word[i]);
- 				freeTokensArray(tokens);
+ 				for (int j = 1; j < wordCount; j++)
+ 					free(tokens[j]);
+ 				free(tokens);
  				return NULL;
  			} 
  			word[i] = tolower(word[i]);
@@ -109,27 +291,17 @@ char **tokenize(char *query)
  		word = strtok(NULL, " ");
  	}
 
- 	char *arrLenStr = malloc(numDigits(wordCount) + 2);
+ 	char *arrLenStr = count_malloc_assert(numDigits(wordCount) + 2, MALLOC_ERROR);
  	sprintf(arrLenStr, "%d", wordCount);
 
- 	tokens[0] = count_malloc_assert(strlen(arrLenStr), MALLOC_ERROR);
+ 	tokens[0] = count_malloc_assert(strlen(arrLenStr) + 1, MALLOC_ERROR);
+ 	
  	// store array length in first position
  	strcpy(tokens[0], arrLenStr);
+ 	count_free(arrLenStr);
 
  	// return pointer to first element of actual array
  	return ++tokens;
- }
-
- static void freeTokensArray(char **tokens)
- {
- 	int arrLen = arrayLength(tokens);
- 	
- 	for (int i = 0; i < arrLen; i++) {
- 		if (tokens[i] != NULL)
- 			count_free(tokens[i]);
- 	}
- 	// must free from start of original allocated array
- 	count_free(tokens - 1);
  }
 
  static bool validateQuery(char **tokens) 
@@ -169,6 +341,25 @@ char **tokenize(char *query)
  }
 
 
+
+
+ static void freeTokensArray(char **tokens)
+ {
+ 	
+	int arrLen = arrayLength(tokens);
+ 	
+ 	for (int i = 0; i < arrLen; i++) {
+ 		if (tokens[i] != NULL)
+ 			count_free(tokens[i]);
+ 	}
+ 	// free the length of array
+ 	count_free(*(tokens - 1));
+ 	// must free from start of original allocated array
+ 	count_free(tokens - 1);
+
+ 	printf("Freed tokens array\n");
+ }
+
 /* parseArguments: check arguments passed to querier
  * returns nonzero if incorrect number of invalid pageDirectory
  * passed. zero otherwise
@@ -198,11 +389,14 @@ char **tokenize(char *query)
  		fprintf(stderr, "See querier.c README.md for index filename reqs\n");
  		return 5;
  	}
+ 	fclose(fp);
  	
  	// arguments valid to go ahead
  	return 0;
 
  }
+
+
 
 
  /*
@@ -261,7 +455,7 @@ static int lines_in_file(FILE *fp)
  * numDigits: recursively calculate
  * the number of digits in given integer
  */
-int numDigits(int number)
+static int numDigits(int number)
 {
 	// integer division checks if 1 digit
 	if ( (number / 10 ) == 0 ) {
@@ -275,7 +469,7 @@ int numDigits(int number)
 /*
  * arrayLength: return size of array
  */
-int arrayLength(char **array)
+static int arrayLength(char **array)
 {
 	char *lengthStr = *(array - 1);
 	int length = strtol(lengthStr, NULL, 10);
